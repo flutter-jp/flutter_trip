a32ca005afa849a0a7afcad696f510b63fc5c321
xiaomo <xiaomo@xiaomo.info>
2020-07-31 18:19:09 +0800

    flutter trip

---
+++ README.md
@@ -0,0 +1,42 @@
+
+
+这是慕课网官方实战课【[Flutter从入门到进阶-实战携程网App](https://coding.imooc.com/class/321.html)】的项目源码，课件，文档。
+
+
+## 概述
+- 课程文档查看[doc](https://git.imooc.com/coding-321/flutter_trip/src/master/doc);
+- 课程中所用到的demo查看[demo](https://git.imooc.com/coding-321/flutter_trip/src/master/demo);
+- 课程源码，可通过git查看课程各章节的源码；
+
+## 课程辅导答疑
+
+[http://coding.imooc.com/learn/qa/321.html](http://coding.imooc.com/learn/qa/321.html)
+
+## 如何运行？
+
+1. 安装和配置Flutter开发环境(如已经配置过可跳过)，可参考课程[Flutter入门：开发工具准备与开发环境搭建](https://coding.imooc.com/class/321.html)一章的讲解。
+2. 在项目根目录运行`flutter run`或用装有Flutter和Dart插件的IDE运行；
+3. Ok,有问题可以在[课程讨论区](https://coding.imooc.com/class/321.html)提issue哦;
+
+## 基础部分辅助文档和代码
+**教程中大部分的辅助文档已通过锚点的方式插入到对应的视频中，可以通过课程目录判断是否有锚点：**
+![doc-learn-help](http://www.devio.org/io/flutter_app/img/blog/doc-learn-help2.jpg) 
+
+>然后通过如下方式查看锚点中的文档：
+
+![doc-learn-help](http://www.devio.org/io/flutter_app/img/blog/doc-learn-help1.jpg)
+
+>另外，部分辅助文档和代码可以通过下面列表查看：
+
+- 第2章 Flutter入门：开发工具准备与开发环境搭建
+	- [Flutter开发环境搭建（Mac）](http://www.imooc.com/article/284328)
+	- [Flutter开发环境搭建（Windows）](http://www.imooc.com/article/284627)
+- 第4章 Flutter入门：Flutter必备基础
+    - [带你轻松掌握Flutter 动画开发核心技能](http://www.imooc.com/article/285177)
+    - [Flutter Hero动画开发实用教程](http://www.imooc.com/article/285381)
+
+
+## 接口文档
+
+- [首页大接口文档](https://git.imooc.com/coding-321/flutter_trip/src/master/doc/首页大接口文档.md)
+
---
+++ demo/flutter_base_demo/lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_base_demo/flutter_layout_page.dart';
+import 'package:flutter_base_demo/gesture_page.dart';
+import 'package:flutter_base_demo/launch_page.dart';
+import 'package:flutter_base_demo/less_group_page.dart';
+import 'package:flutter_base_demo/photo_app_page.dart';
+import 'package:flutter_base_demo/plugin_use.dart';
+import 'package:flutter_base_demo/res_page.dart';
+import 'package:flutter_base_demo/statefull_group_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('如何创建和使用Flutter的路由与导航？'),
+          ),
+          body: RouteNavigator(),
+        ),
+        routes: <String, WidgetBuilder>{
+          'plugin': (BuildContext context) => PluginUse(),
+          'less': (BuildContext context) => LessGroupPage(),
+          'ful': (BuildContext context) => StatefulGroup(),
+          'layout': (BuildContext context) => FlutterLayoutPage(),
+          'gesture': (BuildContext context) => GesturePage(),
+          'res': (BuildContext context) => ResPage(),
+          'launch': (BuildContext context) => LaunchPage(),
+          'photo': (BuildContext context) => PhotoApp(),
+        });
+  }
+}
+
+class RouteNavigator extends StatefulWidget {
+  @override
+  _RouteNavigatorState createState() => _RouteNavigatorState();
+}
+
+class _RouteNavigatorState extends State<RouteNavigator> {
+  bool byName = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          SwitchListTile(
+              title: Text('${byName ? '' : '不'}通过路由名跳转'),
+              value: byName,
+              onChanged: (value) {
+                setState(() {
+                  byName = value;
+                });
+              }),
+          _item('如何使用Flutter包和插件？', PluginUse(), 'plugin'),
+          _item('StatelessWidget与基础组件', LessGroupPage(), 'less'),
+          _item('StatefulWidget与基组件', StatefulGroup(), 'ful'),
+          _item('如何进行Flutter布局开发', FlutterLayoutPage(), 'layout'),
+          _item('如何检测用户手势以及处理点击事件？', GesturePage(), 'gesture'),
+          _item('如何导入和使用Flutter的资源文件？', ResPage(), 'res'),
+          _item('如何打开第三方应用？', LaunchPage(), 'launch'),
+          _item('【实战尝鲜】拍照APP开发', PhotoApp(), 'photo'),
+        ],
+      ),
+    );
+  }
+
+  _item(String title, page, String routeName) {
+    return Container(
+      child: RaisedButton(
+        onPressed: () {
+          if (byName) {
+            Navigator.pushNamed(context, routeName);
+          } else {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => page));
+          }
+        },
+        child: Text(title),
+      ),
+    );
+  }
+}
